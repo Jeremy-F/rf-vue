b6f31ea5414680974b13de798f197158afedd904
jeremy-f <jeremy.fornarino@gmail.com>
2018-04-11 18:29:39 +0200

    change config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const API_ROOT = (process.env.NODE_ENV === 'production')
-    ? 'http://projet.jeremy-f.me/api/'
+    ? 'http://10.0.80.80/api/'
     :'http://localhost:8080/api/';
 
 export const WS_NAME = "SpA";
